9a9ffcda75cdea4d969b50672cce2a6505d0472d
DarkscriptHUBS <yuanagayo01@gmail.com>
2025-07-27 00:04:16 +0800

    Update random.lua

--- random.lua
+++ random.lua
@@ -1,1 +1,3 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/DarkscriptHUBS/wowk/refs/heads/main/loli"))()
+task.wait(1)
 loadstring(game:HttpGet("https://raw.githubusercontent.com/DarkscriptHUBS/Gag-scripts/refs/heads/main/Rnadominzer"))()
